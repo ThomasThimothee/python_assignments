97b9cd45334f87c57dc55f144758f9836cfa1259
Thomas Thimothee <thom.thimothee@gmail.com>
2018-02-24 16:48:27 +0100

    .

--- assignment_1/bg_assignment_1.py
+++ assignment_1/bg_assignment_1.py
@@ -2,6 +2,7 @@ import sys
 import webget
 import xlrd
 import matplotlib.pyplot as plt
+import pprint
 
 
 filename_1 = "data1.xls"
@@ -43,7 +44,7 @@ def question_1():
 
     plt.plot(years, values)
     plt.axis([min(years), max(years), 0, max(values)])
-    plt.title("Evolution of total number of crime", fontsize = 24)
+    plt.title("Question 1: Evolution of total number of crime", fontsize = 24)
     plt.xlabel("years", fontsize = 14)
     plt.ylabel("Total number of crime", fontsize = 14)
     plt.show()
@@ -89,6 +90,8 @@ def question_2():
     plt.title("Crime per category 2013", fontsize = 24)
 
     plt.legend(labels = final_categories, bbox_to_anchor=(1.5, 0), loc='lower left', borderaxespad=0.)
+    
+    plt.suptitle('Question 2')
     plt.show()
     
         
@@ -96,11 +99,75 @@ def question_2():
 def question_3():
     wb = xlrd.open_workbook(filename_2)
     sheet = wb.sheet_by_index(0)
-    
+    state = "ALABAMA"
+    count = 0
+    results = {}
+    #when merge cells (first column, state name) , only first row has value, following ones have empty values
+    # sheet.cell_value(4, 0) = Alabama // sheet.cell_value(5, 0) and followings = ""
+    for row in range(4,9295):  
+        if sheet.cell_value(row, 0) != "":
+            results.update({state : count})
+            state = sheet.cell_value(row, 0) 
+            count = 0     
+            for column in range(3,14):
+                if sheet.cell_value(row,column) != "":
+                    count += int(sheet.cell_value(row,column))
+        else :
+            for column in range(3,14):
+                if sheet.cell_value(row,column) != "":
+                    count += int(sheet.cell_value(row,column))
+
+    plt.bar(results.keys(), results.values())  
+    plt.xlabel('xlabel', fontsize=5)
+    plt.xticks(rotation=90)   
+    plt.title("Question 3") 
+    plt.show()
 
 #   Question 4: Is there a connection between type of crimes and locations?
+# --> look at top 2 crime categories per state
 def question_4():
-    pass
+    results = {}
+    state = "ALABAMA"
+    crimes_categories = {}
+    top ={}
+
+    wb = xlrd.open_workbook(filename_2)
+    sheet = wb.sheet_by_index(0)
+    for row in range(4,9295):  
+        if sheet.cell_value(row, 0) != "":
+            top = top_cat(crimes_categories)
+            results.update({state : top})
+            crimes_categories = {}
+            state = sheet.cell_value(row, 0)      
+            for column in range(3,14):
+                count = crimes_categories.get(sheet.cell_value(3, column),  0)
+                if sheet.cell_value(row,column) != "":
+                    count += int(sheet.cell_value(row,column))
+                crimes_categories[sheet.cell_value(3, column)] = count
+        else :
+            for column in range(3,14):
+                count = crimes_categories.get(sheet.cell_value(3, column),  0)
+                if sheet.cell_value(row,column) != "":
+                    count += int(sheet.cell_value(row,column))
+                crimes_categories[sheet.cell_value(3, column)] = count  
+    print("__________________")
+    print("question 4")
+    print("__________________")
+    pprint.pprint(results)             
+
+def top_cat(dic):
+    my_dic = dic
+    top ={}
+    for _ in range(3):
+        max_cat = ""
+        max_value = 0
+        for cat, value in my_dic.items():
+            if value > max_value:
+                max_value = value
+                max_cat = cat  
+        top[max_cat] = max_value
+        my_dic.pop(max_cat, None)   # del my_dic[max_cat] was throwing an error... don't know why  
+    return(top)
 
 
 #   Question 5: Which year was the most crime and what crime occured most times?
@@ -120,7 +187,10 @@ def question_5():
         year = str(int(sheet.cell_value(row, 0))) 
         years.append(year)
 
-    max_value = max(values)
+    # wanted to use these values to find the year with biggest crime number and do second subplot
+    # dynamically, however it means finding the row where the column 1 equal this value, didn't have the time 
+    #to implement it yet. So hardcode the row 4 in second subplot so far
+    max_value = max(values) 
     max_index = values.index(max_value)
 
 
@@ -136,6 +206,7 @@ def question_5():
     for column in range(2, 19, 2):
         categories.append(sheet.cell_value(3, column))      
     
+
     plt.subplot(211)
     plt.bar(years, values)
     plt.axis([min(years), max(years), 0, max(values)])
@@ -147,6 +218,7 @@ def question_5():
     plt.ylabel("Number of crime", fontsize = 7)
     plt.title("Crimes per category in 1994", fontsize = 12)
     
+    plt.suptitle('Question 5')
     plt.show()
 
 
@@ -155,8 +227,8 @@ def question_5():
 if __name__ == "__main__" :
 #    doc_1(sys.argv[1])
 #    doc_2(sys.argv[2])
-#    question_1()
-#    question_2()
-#    question_3()
-#    question_4()
+    question_1()
+    question_2()
+    question_3()
+    question_4()
     question_5()

fac05b91321a5444d4fef7796c19e9361face605
mathiasjepsen <mathias@wpsnet.com>
2018-03-14 18:34:02 +0100

    Added question 1

--- assignment_4/imdb_questions.py
+++ assignment_4/imdb_questions.py
@@ -10,11 +10,15 @@ imdb_csv = pd.read_table(webget.download(
 
 # Question 1: The 3 most expensive teams and the 3 cheapest teams according to player value.
 def question_1():
-    print(imdb_csv.columns.groupby("startYear"))
-    
-    #top_3 = fifa_csv.groupby('club')['eur_value'].sum().sort_values(ascending=False).head(3)
-    
-    #bottom_3 = fifa_csv.groupby('club')['eur_value'].sum().sort_values(ascending=False).tail(3)
+    top_ten_years = imdb_csv.groupby("startYear")["startYear"].count().sort_values(ascending=False).head(10).plot.bar()
+
+    # Small fix for the bottom of the graph so that the graph doesn't go too much outside the window
+    plt.subplots_adjust(bottom=0.2, left=0.15)
+
+    plt.title("Number of movies released for each year", fontsize=12, y=1.08)
+    plt.xticks(rotation=90)
+    plt.ylabel("Movies released")
+    plt.show()
 
 
 question_1()

5e8678774968efb94ddef5b589d0443941b2a85b
mathiasjepsen <mathias@wpsnet.com>
2018-03-18 19:22:34 +0100

    .

--- assignment_4/imdb_questions.py
+++ assignment_4/imdb_questions.py
@@ -4,19 +4,14 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+
 imdb_csv = pd.read_table(webget.download("https://datasets.imdbws.com/title.basics.tsv.gz"), compression="gzip", sep="\t", dtype={"startYear" : str})
-
-
-# This part is dumb, need to figure out a smarter way of doing it. It basically just removes all the rows with \N in them
-# The 'r' before the string produces a raw string, and without it, the '\N' will be interpreted as an escape character
-imdb_csv = imdb_csv[imdb_csv.startYear != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.endYear != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.runtimeMinutes != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.genres != r"\N"]
 
 # Question 1: Which year was the most movies released?
 def question_1():
-    top_ten_years = imdb_csv.groupby("startYear")["startYear"].count().sort_values(ascending=False).head(10).plot.bar()
+    imdb_csv_filtered = imdb_csv[imdb_csv.startYear != r"\N"]
+
+    top_ten_years = imdb_csv_filtered.groupby("startYear")["startYear"].count().sort_values(ascending=False).head(10).plot.bar()
 
     # Small fix for the bottom of the graph so that the graph doesn't go too much outside the window
     plt.subplots_adjust(bottom=0.2, left=0.15)
@@ -24,11 +19,14 @@ def question_1():
     plt.title("Number of movies released for each year", fontsize=12, y=1.08)
     plt.xticks(rotation=90)
     plt.ylabel("Movies released")
-    plt.show()
+    plt.savefig("imdb_question_1.png")
+
 
 #Question 2: Which year was the most series ended?
 def question_2():
-    top_5 = imdb_csv.groupby("endYear")["endYear"].count().sort_values(ascending=False).head(5).plot.bar()
+    imdb_csv_filtered = imdb_csv[imdb_csv.endYear != r"\N"]
+
+    top_5 = imdb_csv_filtered.groupby("endYear")["endYear"].count().sort_values(ascending=False).head(5).plot.bar()
 
     # Small fix for the bottom of the graph so that the graph doesn't go too much outside the window
     plt.subplots_adjust(bottom=0.2, left=0.15)
@@ -36,32 +34,38 @@ def question_2():
     plt.title("Series by end year", fontsize=12, y=1.08)
     plt.xticks(rotation=90)
     plt.ylabel("Series ended")
-    plt.show()
+    plt.savefig("imdb_question_2.png")
 
     
 # Question 3: Which genres has the longest runtime per movies?
 def question_3():
-    sum_minutes_genres = imdb_csv.groupby("genres")["runtimeMinutes"].sum()
-    count_film_genres = imdb_csv.groupby("genres")["genres"].count()
+    imdb_csv_filtered = imdb_csv[imdb_csv.runtimeMinutes != r"\N"]
+
+    sum_minutes_genres = imdb_csv_filtered.groupby("genres")["runtimeMinutes"].sum()
+    count_film_genres = imdb_csv_filtered.groupby("genres")["genres"].count()
 
     runtime_per_genre = []
     for genre in count_film_genres.index:
         runtime_per_genre.append((genre, round(sum_minutes_genres.loc[genre] / count_film_genres.loc[genre], 2)))
-        
- 
 
 
 # Question 4: Which genre covers the most movies?
 def question_4():
-    top_genres = imdb_csv.groupby("genres")["genres"].count().sort_values(ascending=False).head(15).plot.bar()
-    plt.subplots_adjust(bottom=0.2, left=0.15)
+    imdb_csv_filtered = imdb_csv[imdb_csv.genres != r"\N"]
+
+    top_genres = imdb_csv_filtered.groupby("genres")["genres"].count().sort_values(ascending=False).head(15).plot.bar()
+    plt.subplots_adjust(bottom=0.3, left=0.15)
     plt.title("Movies By genre", fontsize=12, y=1.08)
     plt.xticks(rotation=90)
     plt.ylabel("Movies released")
-    plt.show()
+    plt.savefig("imdb_question_4.png")
+
+
 # Question 5: What is the average runtime on adult films?
 def question_5():
-    avg_runtime_adult = np.asarray(imdb_csv.groupby("isAdult").get_group(1)["runtimeMinutes"]).astype(int).mean()
+    imdb_csv_filtered = imdb_csv[imdb_csv.runtimeMinutes != r"\N"]
+
+    avg_runtime_adult = np.asarray(imdb_csv_filtered.groupby("isAdult").get_group(1)["runtimeMinutes"]).astype(int).mean()
     print(round(avg_runtime_adult, 2))
 
 

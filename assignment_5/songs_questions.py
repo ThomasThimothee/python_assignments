f9157236d93511ede1c7e8362c105d46ec66f7ff
mathiasjepsen <mathias@wpsnet.com>
2018-03-27 16:19:51 +0200

    Initial initial commit

--- assignment_5/songs_questions.py
+++ assignment_5/songs_questions.py
@@ -4,71 +4,29 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-imdb_csv = pd.read_table(webget.download("https://datasets.imdbws.com/title.basics.tsv.gz"), compression="gzip", sep="\t", dtype={"startYear" : str})
+songs_csv = pd.read_csv(webget.download("https://www.kaggle.com/mousehead/songlyrics/downloads/songdata.csv/1")
 
-
-# This part is dumb, need to figure out a smarter way of doing it. It basically just removes all the rows with \N in them
-# The 'r' before the string produces a raw string, and without it, the '\N' will be interpreted as an escape character
-imdb_csv = imdb_csv[imdb_csv.startYear != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.endYear != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.runtimeMinutes != r"\N"]
-imdb_csv = imdb_csv[imdb_csv.genres != r"\N"]
-
-# Question 1: Which year was the most movies released?
+# Question 1: What is the most used words in the songs?
 def question_1():
-    top_ten_years = imdb_csv.groupby("startYear")["startYear"].count().sort_values(ascending=False).head(10).plot.bar()
 
-    # Small fix for the bottom of the graph so that the graph doesn't go too much outside the window
-    plt.subplots_adjust(bottom=0.2, left=0.15)
 
-    plt.title("Number of movies released for each year", fontsize=12, y=1.08)
-    plt.xticks(rotation=90)
-    plt.ylabel("Movies released")
-    plt.show()
-
-#Question 2: Which year was the most series ended?
+#Question 2: How many times are each word repeated in a song?
 def question_2():
-    top_5 = imdb_csv.groupby("endYear")["endYear"].count().sort_values(ascending=False).head(5).plot.bar()
-
-    # Small fix for the bottom of the graph so that the graph doesn't go too much outside the window
-    plt.subplots_adjust(bottom=0.2, left=0.15)
-
-    plt.title("Series by end year", fontsize=12, y=1.08)
-    plt.xticks(rotation=90)
-    plt.ylabel("Series ended")
-    plt.show()
 
     
-# Question 3: Which genres has the longest runtime per movies?
+# Question 3: What song uses the word "X" the most time? (X meaning a specific word, choose your own!)
 def question_3():
-    sum_minutes_genres = imdb_csv.groupby("genres")["runtimeMinutes"].sum()
-    count_film_genres = imdb_csv.groupby("genres")["genres"].count()
-
-    runtime_per_genre = []
-    for genre in count_film_genres.index:
-        runtime_per_genre.append((genre, round(sum_minutes_genres.loc[genre] / count_film_genres.loc[genre], 2)))
-        
- 
 
 
-# Question 4: Which genre covers the most movies?
+# Question 4: What is the average number of words per song?
 def question_4():
-    top_genres = imdb_csv.groupby("genres")["genres"].count().sort_values(ascending=False).head(15).plot.bar()
-    plt.subplots_adjust(bottom=0.2, left=0.15)
-    plt.title("Movies By genre", fontsize=12, y=1.08)
-    plt.xticks(rotation=90)
-    plt.ylabel("Movies released")
-    plt.show()
-# Question 5: What is the average runtime on adult films?
+
+
+# Question 5: Show the distribution of number of words in the songs. (Example: how many songs have 5-10 words, 10-20 words)
 def question_5():
-    avg_runtime_adult = np.asarray(imdb_csv.groupby("isAdult").get_group(1)["runtimeMinutes"]).astype(int).mean()
-    print(round(avg_runtime_adult, 2))
 
 
-question_1()
-question_2()
-question_4()
-question_5()
+
 
 
 

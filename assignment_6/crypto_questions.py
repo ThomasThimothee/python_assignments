d1c99e336bf87cb50788cbf52949a7140d4fa72a
mathiasjepsen <mathias@wpsnet.com>
2018-04-07 10:49:31 +0200

    Initial request formatted

--- assignment_6/crypto_questions.py
+++ assignment_6/crypto_questions.py
@@ -1,8 +1,13 @@
 import webget
 import pandas as pd
 import numpy as np
+import requests
 import matplotlib.pyplot as plt
 from collections import Counter
+
+
+r = np.asarray(requests.get("https://rest.coinapi.io/v1/assets?output_format=csv", headers={"X-CoinAPI-Key": "2B40C084-9AFF-4682-A715-CE322A8B2DDB"}))
+
 
 
 # Question 1: 
@@ -30,7 +35,7 @@ def question_5():
     pass
         
 
-#question_1()
+question_1()
 #question_2()
 #question_3()
 #question_4()

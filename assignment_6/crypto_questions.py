21274fc06a8a3aa244d6aefd7954c14ff7cababf
mathiasjepsen <mathias@wpsnet.com>
2018-04-09 16:01:13 +0200

    Added Q3

--- assignment_6/crypto_questions.py
+++ assignment_6/crypto_questions.py
@@ -7,20 +7,9 @@ from collections import Counter
 import json
 import csv
 
-#Execute get request and receive response
-url = "https://rest.coinapi.io/v1/trades/COINBASE_SPOT_BTC_USD/history?time_start=2018-04-01T00:00:00&time_end=2018-04-07T00:00:00"
-headers = {'X-CoinAPI-Key': '776A95A6-4A95-4A22-9A07-88BF52F2888E'}
-response = requests.get(url, headers=headers)
 
-#Transform response (Json formatted) into a list of dictionaries
-jsonDataAsPythonValue = json.loads(response.content)
+crypto_csv = pd.read_table(webget.download("https://raw.githubusercontent.com/PeterL93/PythonProject/master/trades_march_to_april_2018.csv"), sep=";")
 
-#Write the list of dictionaries into a csv file we can manipulate later on
-keys = list(jsonDataAsPythonValue[0].keys())
-with open('trades.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, fieldnames=keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(jsonDataAsPythonValue)
 
 # Question 1: 
 def question_1():
@@ -34,7 +23,7 @@ def question_2():
 
 # Question 3: 
 def question_3():
-    pass
+    print(crypto_csv["taker_side"].value_counts())
 
 
 # Question 4: 
@@ -49,7 +38,7 @@ def question_5():
 
 #question_1()
 #question_2()
-#question_3()
+question_3()
 #question_4()
 #question_5()
 

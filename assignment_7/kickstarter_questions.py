1b2e0f4bc6cce42934ebfe0570c5fe12a69a6bfc
mathiasjepsen <mathias@wpsnet.com>
2018-04-13 19:17:01 +0200

    Added question 4

--- assignment_7/kickstarter_questions.py
+++ assignment_7/kickstarter_questions.py
@@ -10,13 +10,10 @@ kickstarter_csv = pd.read_csv(webget.download("https://github.com/mathiasjepsen/
 # Question 1: What main-category of project has the highest success rate?
 def question_1():
     mask = kickstarter_csv.state == "successful"
-    success_rates = kickstarter_csv[mask].groupby("main_category")["state"].value_counts().plot.bar()
+    success_rates = kickstarter_csv[mask]["main_category"].value_counts().plot.bar()
 
-    # TO DO - Fix the xtick values, so that it's only the category and not a tuple of the category + the string 'successful'
-
-    plt.subplots_adjust(bottom=0.2, left=0.15)
+    plt.subplots_adjust(bottom=0.3, left=0.15)
     plt.title("", fontsize=12, y=1.08)
-    plt.xticks(rotation=90)
     plt.ylabel("")
     plt.show()
 
@@ -31,11 +28,9 @@ def question_2():
 
 # Question 3: What is the median pledged amount (usd_pledged_real) of successfully funded projects?
 def question_3():
-    # Question 3: What is the median pledged amount (usd_pledged_real) of successfully funded projects?
-def question_3():
     mask = kickstarter_csv.state == "successful"
-    df_by_usd_pledged = kickstarter_csv[mask].groupby("state")["usd_pledged_real"].median()
-    print(df_by_usd_pledged)
+    median_pledged = kickstarter_csv[mask].groupby("state")["usd_pledged_real"].median()
+    print(median_pledged)
 
     #mask = kickstarter_csv.state == "successful"
     #df_by_usd_pledged = kickstarter_csv[mask].groupby(['usd_pledged_real', 'state']).median()
@@ -44,7 +39,13 @@ def question_3():
 
 # Question 4: What is the number of successfully funded projects with more than 5.000$ pledged (usd_pledged_real) per category?
 def question_4():
-    pass
+    mask = (kickstarter_csv.usd_pledged_real > 5000) & (kickstarter_csv.state == "successful")
+    kickstarter_csv[mask]["category"].value_counts().head(10).plot.bar()
+
+    plt.subplots_adjust(bottom=0.3, left=0.15)
+    plt.title("", fontsize=12, y=1.08)
+    plt.ylabel("")
+    plt.show()
 
 
 # Question 5: For the main-category with the most successfully funded projects (quantity, not rate of success), 
@@ -54,7 +55,7 @@ def question_5():
     pass
         
 
-#question_1()
+question_1()
 #question_2()
 question_3()
 question_4()
